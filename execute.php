f7b823870c9d86b6a08fcb702dd2ae9f0bf1290c
vannipluto <vannipluto@gmail.com>
2021-02-17 18:04:49 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,51 +1,51 @@
 <?php
-////$content = file_get_contents("php://input");
-////$update = json_decode($content, true);
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
 
-////if(!$update)
-////{
-////  exit;
-////}
+if(!$update)
+{
+  exit;
+}
 
-////define("BOT_TOKEN", "630788166:AAGJ0s62LlXbEOQmMOaUi-bRgVTYVZLPdEs");
+define("BOT_TOKEN", "630788166:AAGJ0s62LlXbEOQmMOaUi-bRgVTYVZLPdEs");
 
-////$message = isset($update['message']) ? $update['message'] : "";
-////$messageId = isset($message['message_id']) ? $message['message_id'] : "";
-////$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
-////$firstname = isset($message['from']['first_name']) ? $message['from']['first_name'] : "";
-////$lastname = isset($message['from']['last_name']) ? $message['from']['last_name'] : "";
-////$username = isset($message['from']['username']) ? $message['from']['username'] : "";
-////$date = isset($message['date']) ? $message['date'] : "";
-////$text = isset($message['text']) ? $message['text'] : "";
+$message = isset($update['message']) ? $update['message'] : "";
+$messageId = isset($message['message_id']) ? $message['message_id'] : "";
+$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
+$firstname = isset($message['from']['first_name']) ? $message['from']['first_name'] : "";
+$lastname = isset($message['from']['last_name']) ? $message['from']['last_name'] : "";
+$username = isset($message['from']['username']) ? $message['from']['username'] : "";
+$date = isset($message['date']) ? $message['date'] : "";
+$text = isset($message['text']) ? $message['text'] : "";
 
-////$text = trim($text);
-////$text = strtolower($text);
+$text = trim($text);
+$text = strtolower($text);
 
-////if($text == "/start") {
+if($text == "/start") {
   // start bot esco e non inoltro
-  ////exit;
-////}
+  exit;
+}
 
-////$bannedStick = array("a954024537_by_sbirrunoob_1","shurhandfp2", "ahegaoni", "DBDnapoli", "follettinas", "Pklspk", "KawaiiDeath", "VkFace", "CheSchifoPack", "Ginofiga", "ITAStickers", "Salvini", "chescifo", "Animushit", "Sexsting_NBstickeria", "Matteo_Salvini");
+$bannedStick = array("a954024537_by_sbirrunoob_1","shurhandfp2", "ahegaoni", "DBDnapoli", "follettinas", "Pklspk", "KawaiiDeath", "VkFace", "CheSchifoPack", "Ginofiga", "ITAStickers", "Salvini", "chescifo", "Animushit", "Sexsting_NBstickeria", "Matteo_Salvini");
 
-////if(isset($message['sticker'])) {
+if(isset($message['sticker'])) {
   
    // verifico lo stickers
-   ////$setName = $message['sticker']['set_name'];
+   $setName = $message['sticker']['set_name'];
   
-   ////if (in_array($setName, $bannedStick)) {
-      ///$botUrl = "https://api.telegram.org/bot" . BOT_TOKEN . "/deleteMessage";
+   if (in_array($setName, $bannedStick)) {
+      $botUrl = "https://api.telegram.org/bot" . BOT_TOKEN . "/deleteMessage";
 
-      ///$postFieldsForDelete = array('chat_id' => $chatId, 'message_id' => $messageId);
+      $postFieldsForDelete = array('chat_id' => $chatId, 'message_id' => $messageId);
 
-      ///$ch = curl_init(); 
-      ///curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type:multipart/form-data"));
-      ///curl_setopt($ch, CURLOPT_URL, $botUrl); 
-      ///curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-      ///curl_setopt($ch, CURLOPT_POSTFIELDS, $postFieldsForDelete);
+      $ch = curl_init(); 
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type:multipart/form-data"));
+      curl_setopt($ch, CURLOPT_URL, $botUrl); 
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $postFieldsForDelete);
      
       // read curl response
-      ///$output = curl_exec($ch);
+      $output = curl_exec($ch);
      
       // $textOut = $firstname . ' ' . $lastname . ' hai usato uno stikers non ammesso in questo gruppo (da ' . $setName . ')! (cancellato)';
 
@@ -54,8 +54,8 @@
       // $parameters["method"] = "sendMessage";
       // echo json_encode($parameters);
      
-   ////}
+   }
   
-////}
+}
 
 ?>
